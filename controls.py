dde00f429bb4d708ddf3188087e8524327a76021
zflink <43156707+zflink@users.noreply.github.com>
2018-11-12 18:58:42 -0500

    update - 11/12/18 ZF

--- controls.py
+++ controls.py
@@ -3,6 +3,7 @@ import time
 import RPi.GPIO as GPIO
 from MicrobeScope import image_processing
 import datetime
+import numpy as np
 
 # Pin Setup:
 LED = 1 #Pin#
@@ -13,38 +14,51 @@ GPIO.setup(LED, GPIO.OUT)
 GPIO.output(LED, False)
 GPIO.output(VALVE, False)
 
-camera = PiCamera()
-camera.resolution = (3200, 2400)
-camera.awb_mode = 'off'
-camera.awb_gains = (1, 1)
 
-
-def ImCapture(self):
+def im_capture():
     GPIO.output(LED, True)
-    time.sleep(1)
     GPIO.output(VALVE, True)
     time.sleep(Sample.valve_time)
     GPIO.output(VALVE, False)
     time.sleep(Sample.valve_time)
-    camera.capture('/home/pi/Desktop/CameraTest/image%s.data' % i, 'rgb')
+    with picamera.PiCamera() as camera:
+        camera.resolution = (3280, 2464)
+        camera.awb_mode = 'off'
+        camera.awb_gains = (1, 1)
+        output = np.empty((2464, 3296, 3), dtype=np.uint8)
+        camera.capture(output, 'rgb')
+        output = output[:, :3280, :] # strip off uninitialized pixels
+    return output
 
-class Sample:
-    def __init__(self):
+
+class Sample(sample_frequency, logfile):
+    def __init__(self, sample_frequency, logfile):
         self.microbe_count = 0
         self.start_time = datetime.datetime.now()
         self.pics = []
         self.frames = 0
+        self.sample_frequency = sample_frequency
+        self.logfile = logfile
 
     def add_pic(self):
-        pass
+        new_pic = im_capture()
+        self.pics.append(new_pic)
+        self.frames += 1
+        self.microbe_count += image_processing(new_pic)
 
-    def analyze_image(self):
-        image_processing.
-
-    def log(self, filename):
-        file_object = open(filename, 'a')
+    def log(self):
+        filename = self.logfile
+        file_object = open('%s.txt' % filename, 'a')
         now = datetime.datetime.now()
         now_string = now.strftime("%Y-%m-%d %H:%M:%S:%f")
         file_object.write(now_string)
-        file_object.write(' - Result:%d ecoli, ' % self.microbe_count + '\n')
+        file_object.write(' - Result:%d microbes, ' % self.microbe_count + '\n')
         file_object.close()
+
+    def sample_run(self):
+        loop_time = time.time()
+        while (time.time() - loop_time) < self.sample_time and self.microbe_count < 400:
+            self.add_pic()
+        while (time.time() - loop_time) < self.sample_time:
+            time.sleep(0.1)
+        self.log()
--- run.py
+++ run.py
@@ -8,20 +8,14 @@ parser = argparse.ArgumentParser(description='Gather input parameters for Microb
 parser.add_argument('-f', '--sample_frequency', help='number of samples per hour', type=int,
                     choices=range(1, 48), nargs='?', const=2)
 parser.add_argument('-s', '--save_images', help='save images to file?', type=bool)
+parser.add_argument('-f', '--logfile_name', help='Provide name for output logfile', type=str)
 
 args = parser.parse_args()
-frequency = args.sample_frequency
 
 try:
     print('Press CTRL+C to quit')
-    count = 0
     while True:
-        loop_time = time.time()
-        while (time.time()-loop_time) < 10 and count < 2000:
-                count += main()
-                print(count)
-        while (time.time()-loop_time) < 10:
-            time.sleep(0.1)
-        count = 0
+        sample = controls.Sample(args.sample_frequency, args.logfile_name)
+        sample.sample_run()
 except KeyboardInterrupt:
     pass
